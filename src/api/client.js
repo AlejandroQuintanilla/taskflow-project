32a20a9fbf458a2ce3aeae6d9ec6cb5edef48885
Alejandro <alejandro.quintanilla@alu.ceacfp.es>
2026-04-27 11:33:06 +0200

    actualizar URL API a Vercel

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api/v1/tasks';
+const API_URL = 'https://taskflow-project-4f6g.vercel.app/api/v1/tasks';
 
 async function obtenerTareas() {
   const res = await fetch(API_URL);
